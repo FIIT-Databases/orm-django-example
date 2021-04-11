0c39529c34acaef7ef1b24e719b076261785a5a5
Jakub Dubec <jakub.dubec@gmail.com>
2021-04-11 21:25:36 +0200

    Purge!!!

--- apps/core/examples/basics.py
+++ apps/core/examples/basics.py
@@ -13,9 +13,7 @@ def select_all() -> List[Member]:
     Select all users in table members using model Member
     :return:
     """
-
-    members = Member.objects.all()
-    return members
+    pass
 
 
 def select_one() -> Member:
@@ -23,11 +21,7 @@ def select_one() -> Member:
     Return exactly one member (if possible) with surname `Poliačik`
     :return:
     """
-    member = Member.objects.get(
-        surname="Poliačik"
-    )
-
-    return member
+    pass
 
 
 def select_first() -> Member:
@@ -35,8 +29,7 @@ def select_first() -> Member:
     Select first record from table parties
     :return:
     """
-    party = Party.objects.first()
-    return party
+    pass
 
 
 def select_youngest() -> Member:
@@ -44,8 +37,7 @@ def select_youngest() -> Member:
     Select the youngest member of parliament ever
     :return:
     """
-    member = Member.objects.order_by('-born_at').first()
-    return member
+    pass
 
 
 def create_party() -> Party:
@@ -53,12 +45,7 @@ def create_party() -> Party:
     Create party with name: `Friends of Douglas Adams`
     :return:
     """
-    party = Party.objects.create(
-        name='Friends of Douglas Adams',
-        color='darkblue'
-    )
-
-    return party
+    pass
 
 
 def party_exists() -> bool:
@@ -66,23 +53,15 @@ def party_exists() -> bool:
     Is there a party with name `Združenie robotníkov Slovenska`?
     :return:
     """
-    return Party.objects.filter(name='Združenie robotníkov Slovenska').exists()
+    pass
 
 
 def longest_party_name() -> str:
-    party_name = Party.objects.annotate(
-        name_length=Length('name')
-    ).order_by('-name_length').first().name
-
-    return party_name
+    pass
 
 
 def most_experienced_member() -> Member:
-    member = Member.objects.annotate(
-        experience=Count('government_members__id')
-    ).order_by('-experience').first()
-
-    return member
+    pass
 
 
 def communists_or_socialists() -> List[Party]:
@@ -90,45 +69,23 @@ def communists_or_socialists() -> List[Party]:
     Select all parties where type is SOCIALISTS or LIBERAL
     :return:
     """
-    parties = Party.objects.filter(
-        type__in=[Party.PartyEnum.SOCIALISTS, Party.PartyEnum.COMMUNIST]
-    )
-
-    parties = Party.objects.filter(
-        Q(type=Party.PartyEnum.SOCIALISTS) | Q(type=Party.PartyEnum.COMMUNIST)
-    )
-
-    return parties
+    pass
 
 
 def communists_or_socialists_with_robo() -> List[Party]:
-    parties = Party.objects.filter(
-        (Q(type=Party.PartyEnum.SOCIALISTS) | Q(type=Party.PartyEnum.COMMUNIST)) &
-        Q(government_members__member__name='Robert')
-    ).distinct('name')
-
-    return parties
+    pass
 
 
 def best_tourist() -> Member:
-    member = Member.objects.annotate(
-        travels=Count('government_members__party_id', distinct=True)
-    ).order_by('-travels').first()
-
-    return member
+    pass
 
 
 def party_type_for_mazurek() -> str:
-    return Party.objects.filter(
-        government_members__member__name='Milan',
-        government_members__member__surname='Mazurek').values_list('type', flat=True)[0]
+    pass
 
 
 def disco_members() -> List[Member]:
-    return Member.objects.filter(
-        born_at__gte=date(1960, 1, 1),
-        born_at__lte=date(1969, 12, 31),
-    )
+    pass
 
 
 def select_raw() -> List[dict]:
